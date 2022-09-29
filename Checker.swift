9e81483e03252d93980557419e4d28c735f43e22
Alexey08 <bauer080@gmail.com>
2022-09-29 22:18:00 +0500

    code cleanup

---
+++ Checker.swift
@@ -0,0 +1,20 @@
+//
+//  Checker.swift
+//  Navigation
+//
+//  Created by Алексей Уланов on 28.09.2022.
+//
+
+import Foundation
+
+
+class Checker {
+    
+    private var login = String()
+    private var password = String()
+    
+    var shared = Checker()
+    
+    private init() {}   
+    
+}
---
+++ User.swift
@@ -0,0 +1,53 @@
+//
+ //  User.swift
+ //  Navigation
+ //
+ //  Created by Алексей Уланов on 03.08.2022.
+ //
+
+ import Foundation
+ import UIKit
+
+ public protocol UserService {
+
+     func userSearch (name: String) -> User?
+ }
+
+ public final class User {
+
+     var name: String
+     var avatar: String?
+     var status: String?
+
+     init (name: String, avatar: String, status: String) {
+         self.name = name
+         self.avatar = avatar
+         self.status = status
+     }
+ }
+
+ public final class CurrentUserService: UserService {
+
+     var user = User(name: "Ivan", avatar: "user_Ivan", status: "Hello world")
+
+     public func userSearch(name: String) -> User? {
+         if name == user.name {
+             return user
+         } else {
+             return User(name: "Test", avatar: "avatar", status: "Test")
+         }
+     }
+ }
+
+ public final class TestUserService: UserService {
+
+     var user = User(name: "Oleg", avatar: "user_Oleg", status: "Motorcyclist")
+
+     public func userSearch(name: String) -> User? {
+         if name == user.name {
+             return user
+         } else {
+             return User(name: "Test", avatar: "avatar", status: "Test")
+         }
+     }
+ }
